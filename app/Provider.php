ebec8a28b6ac7d6bb0f103c44bc2555371d2c890
mimoyasr <amryasserhamza@gmail.com>
2018-05-26 14:46:24 +0200

    update Provider model

--- app/Provider.php
+++ app/Provider.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Provider extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'prefix',
+    ];
+
 }
