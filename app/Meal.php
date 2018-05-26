879c5746b8b3850fe3a26ee628310340044503ba
mimoyasr <amryasserhamza@gmail.com>
2018-05-26 14:41:00 +0200

    update Meal model

--- app/Meal.php
+++ app/Meal.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meal extends Model
 {
-    //
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'chef_id', 
+        'category_id',
+        'image',
+        'perpration_time',
+        'price',
+        'desc',
+    ];
+
+    public function chef()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
