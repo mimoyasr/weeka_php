13d13577f7d905b070f09f3431067682f72e9647
mimoyasr <amryasserhamza@gmail.com>
2018-05-26 14:45:10 +0200

    update Payment model

--- app/Meal.php
+++ app/Meal.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meal extends Model
 {
-     /**
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'user_id', 
+        'type',
+        'card_no',
+        'exp',
+        'cvv',
+        'card_holder_name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
