f2cb4bc74af4c6c5cf1777dfd50789f01731c443
rowaina <e.rowainaahdy@gmail.com>
2018-05-26 11:51:58 +0200

    add telephones table

---
+++ database/migrations/2018_05_26_095128_create_telephones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTelephonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telephones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('number');
+            $table->enum('isactive',['0','1']);
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('provider_id');
+            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telephones');
+    }
+}
