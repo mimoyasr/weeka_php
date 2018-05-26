a1e0177da48dc8054b3478f7bd3bbdd8182a4737
rowaina <e.rowainaahdy@gmail.com>
2018-05-26 13:35:27 +0200

    add favs table

---
+++ database/migrations/2018_05_26_113349_create_favs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('meal_id');
+            $table->foreign('meal_id')->references('id')->on('meals');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favs');
+    }
+}
