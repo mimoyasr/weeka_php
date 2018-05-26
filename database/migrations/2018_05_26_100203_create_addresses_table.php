aaa23d676cb3f67a8b0f72168535f7caa11a5fdf
rowaina <e.rowainaahdy@gmail.com>
2018-05-26 12:12:07 +0200

    add address table

---
+++ database/migrations/2018_05_26_100203_create_addresses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('country_id');
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->unsignedInteger('city_id');
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->unsignedInteger('district_id');
+            $table->foreign('district_id')->references('id')->on('districts');
+            $table->string('name');
+            $table->string('street');
+            $table->integer('buildingno');
+            $table->integer('floorno');
+            $table->integer('flatno');
+            $table->string('notice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
