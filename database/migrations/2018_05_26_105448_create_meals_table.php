4ef454b16271c15639d5f8223172e09bc6e7b9a7
rowaina <e.rowainaahdy@gmail.com>
2018-05-26 12:58:55 +0200

    add meals table

---
+++ database/migrations/2018_05_26_105448_create_meals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('working_meals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('chef_id');
+            $table->foreign('chef_id')->references('id')->on('users');
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('image')->nullable()->default(null);
+            $table->float('prepartion_time');
+            $table->float('price');
+            $table->longText('desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('working_meals');
+    }
+}
