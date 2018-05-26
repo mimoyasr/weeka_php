bb309c28a52e88e1c6c66bff4668843bbab8d053
mimoyasr <amryasserhamza@gmail.com>
2018-05-26 15:04:13 +0200

    update scheduled, Working Hour  model

--- app/Working_hour.php
+++ app/Working_hour.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Working_hour extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'from_hour',
+        'from_min',
+        'from_period',
+        'to_hour',
+        'to_min',
+        'to_period',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
