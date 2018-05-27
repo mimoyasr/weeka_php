555a1f7741f2c9b14ca7bb172f485c71c247beb0
mimoyasr <amryasserhamza@gmail.com>
2018-05-27 13:06:48 +0200

    add chef controller

---
+++ app/Http/Controllers/ChefController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ChefCollection;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Http\Request;
+
+class ChefController extends Controller
+{
+
+    public function index()
+    {
+        $Chefs = User::where('type', 'chef')
+            ->orderBy('fname', 'asc')
+            ->get();
+        return ChefCollection::collection($Chefs);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            $data['image'] = $filename;
+            $destinationPath = public_path('uploads');
+            if (!$image->move($destinationPath, $filename)) {
+                return json_encode([
+                    'Error' => 'Error saving the profile image',
+                ]);
+            }
+        }
+        $data['password'] = bcrypt($request->password);
+        $data['type'] = 'chef';
+        $chef = User::create($data);
+        return new ChefCollection($chef);
+    }
+
+    public function show(User $chef)
+    {
+        return new ChefCollection($chef);
+    }
+
+    public function update(Request $request, User $chef)
+    {
+        $data = $request->all();
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            $data['image'] = $filename;
+            $destinationPath = public_path('uploads');
+            if (!$image->move($destinationPath, $filename)) {
+                return \json_encode(['Error' => 'Error saving the profile image']);
+            }
+        }
+        $data['password'] = bcrypt($request->password);
+        $chef->update($data);
+        return new ChefCollection($chef);
+    }
+
+    public function destroy(User $chef)
+    {
+        return json_encode([
+            "status" => $chef->delete(),
+        ]);
+    }
+}
