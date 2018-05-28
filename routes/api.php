ba4488c3ca93d628c6bec9622d88f9b7b9191587
mimoyasr <amryasserhamza@gmail.com>
2018-05-28 14:13:46 +0200

    fix api routes

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
 Route::resource('chefs', 'ChefController')->except(['create','edit']);
 Route::resource('clients', 'ClientController')->except(['create','edit','index']);
 Route::resource('addresses', 'AddressController')->except(['create','edit','index']);
 Route::resource('telephones', 'TelephoneController')->except(['create','edit','index']);
 Route::resource('workinghours', 'WorkingHourController')->except(['create','edit','index']);
-=======
-Route::resource('chefs', 'ChefController')->except(['create', 'edit']);
-Route::resource('clients', 'ClientController')->except(['create', 'edit', 'index']);
 Route::resource('categories', 'CategoryController')->only(['index', 'show']);
-Route::resource('addresses', 'AddressController')->except(['create', 'edit', 'index']);
->>>>>>> a4abc06eedea187f11e615e0c7226f5e6ebd0271
