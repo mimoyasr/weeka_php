61c3d894283e182a77df1b4a93c7ec512715c030
mimoyasr <amryasserhamza@gmail.com>
2018-06-11 11:08:37 +0200

    fix bugs

--- app/Http/Resources/ProviderResource.php
+++ app/Http/Resources/ProviderResource.php
@@ -15,6 +15,7 @@ class ProviderResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'prefix' => $this->prefix
         ];
