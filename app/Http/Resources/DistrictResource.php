ba3d604b03735956425b1834b3bc600c8104ee1b
mimoyasr <amryasserhamza@gmail.com>
2018-06-12 14:54:33 +0200

    update disttricts

--- app/Http/Resources/DistrictResource.php
+++ app/Http/Resources/DistrictResource.php
@@ -15,7 +15,9 @@ class DistrictResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name' => $this->name
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug 
         ];
     }
 }
