0d976ca9b94b7049a0fbd15b4d4a23822595ef38
mimoyasr <amryasserhamza@gmail.com>
2018-06-14 12:50:30 +0200

    fix menu/{slug} again 3

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,11 @@ use App\Http\Resources\MealResource;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
- */\
+ */
 
+Route::get('menu/{slug}',function(string $slug){
+    app('App\Http\Controllers\MenuController')->show("hamada", $slug);
+});
 Route::resource('countries', 'CountryController')->only(['index']);
 Route::resource('districts', 'DistrictController')->only(['index']);
 Route::resource('providers', 'ProviderController')->only(['index']);
@@ -27,9 +30,6 @@ Route::resource('cities', 'CityController')->only(['index']);
 Route::resource('categories', 'CategoryController')->only(['index', 'show']);
 Route::post('login', 'LoginController@login');
 Route::post('forgetpassword', 'ForgotPasswordController@update')->name('forgot.password');
-Route::get('menu/{slug}',function(string $slug){
-    app('App\Http\Controllers\MenuController')->show("hamada", $slug);
-});
 Route::post('hamada', function () {
     echo " i am hamada";
 })->name('password.reset');
